d7c4ff2f066bacb1d9fc519585c75d773de2be6e
brandonruizmora <brandon_ruizmor@outlook.es>
2021-08-31 21:24:50 -0500

    Test suit for custom hook

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -7,22 +7,28 @@ describe('Pruebas en el hook useFetchGifs', () => {
         //Arrange
         const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('One Punch') );
         const { data, loading } = result.current;
+
         //Act
         await waitForNextUpdate({timeout:1500});
+
         //Assert
         expect( data ).toEqual([]);
         expect( loading ).not.toBeFalsy();
+
     });
 
     test('Debe retornar un arreglo de imgs y el loading en false', async () => {
         //Arrage
         const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('One Punch') );
+
         //Act
         await waitForNextUpdate({timeout:1500});
         const { data, loading } = result.current;
+
         //Assert
         expect( data.length ).toBe(10);
         expect( loading ).toBeFalsy();
+        
     });
     
 });
